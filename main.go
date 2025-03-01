e0c06cc7851afc8486e8fd08a6cb25e860a26d81
NaonYutakachan <naon.yutakachan@gmail.com>
2025-03-01 20:52:57 +0900

    絵合わせゲーム [実装] Go言語によるマルチプレクサの実装，html等の配置と記述を変更
    - `main.go`
        - サーバーサイドの実装を追加
    - `html`, `css`, `js` ファイルを `game` ディレクトリに移動，改名
    - `game\display.html`
        - 内容を `main.go` で呼び出すテンプレートファイル用に変更

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"text/template"
+)
+
+func processGame(writer http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles("game/display.html"))
+	t.ExecuteTemplate(writer, "display", "(可変メッセージ)")
+}
+
+func main() {
+	// マルチプレクサを用意する．
+	mux := http.NewServeMux()
+
+	// 表示するファイルを取得する．
+	files := http.FileServer(http.Dir("game"))
+	mux.Handle("/static/", http.StripPrefix("/static/", files))
+
+	// ハンドラを登録する．
+	mux.HandleFunc("/game", processGame)
+
+	// サーバーを起動する．
+	server := &http.Server{
+		Addr:    "127.0.0.1:8080",
+		Handler: mux,
+	}
+	fmt.Print("Running server...")
+	server.ListenAndServe()
+}

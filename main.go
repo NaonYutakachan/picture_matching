2ab2e9212bb603fa9d072b51b3513027407d6000
NaonYutakachan <naon.yutakachan@gmail.com>
2025-03-18 11:26:45 +0900

    絵合わせゲーム [実装] 設定ファイルの追加，ハンドラの追加 - config.json     - 新規追加     - サーバーのアドレスを設定する - main.go     - 設定ファイルを読み出す動作を追加     - タイトル画面用のハンドラを追加(実装予定)

--- main.go
+++ main.go
@@ -1,10 +1,38 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"text/template"
 )
+
+type Configuration struct {
+	Address string
+}
+
+// config.json からプログラム実行時の設定を読み出し，構造体に格納して戻り値として返す．
+//
+// 成功時は構造体を返し，失敗時は nil とエラーを返す．
+func loadConfig() (*Configuration, error) {
+	// 設定ファイルを読み出す．
+	file, err := os.Open("config.json")
+	if err != nil {
+		return nil, err
+	}
+
+	// 読み出した設定データを，構造体に格納する．
+	config := Configuration{}
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
 
 func processGame(writer http.ResponseWriter, request *http.Request) {
 	t := template.Must(template.ParseFiles("game/display.html"))
@@ -12,19 +40,26 @@ func processGame(writer http.ResponseWriter, request *http.Request) {
 }
 
 func main() {
+	// 設定を読み出す．
+	config, err := loadConfig()
+	if err != nil {
+		log.Fatalln("Cannot get configuration from file", err)
+	}
+
 	// マルチプレクサを用意する．
 	mux := http.NewServeMux()
 
-	// 表示するファイルを取得する．
+	// ウェブサイト表示に用いるファイル群を取得する．
 	files := http.FileServer(http.Dir("game"))
 	mux.Handle("/game/", http.StripPrefix("/game/", files))
 
 	// ハンドラを登録する．
+	mux.HandleFunc("/", processGame) // TODO: タイトル画面の表示
 	mux.HandleFunc("/game", processGame)
 
 	// サーバーを起動する．
 	server := &http.Server{
-		Addr:    "127.0.0.1:8080",
+		Addr:    config.Address,
 		Handler: mux,
 	}
 	fmt.Print("Running server...")
